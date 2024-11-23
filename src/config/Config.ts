b3d44acec9be93ffbf60a67c72b4da746439daba
RajulThakur <139610927+RajulThakur@users.noreply.github.com>
2024-11-23 21:19:47 +0530

    added url

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -1,6 +1,6 @@
 export const BASE_URL: string = "/api/v1";
 export const corsOptions = {
-  origin: ["http://localhost:5173", "https://brainly-100x.vercel.app"],
+  origin: ["http://localhost:5173", "https://brainly-100x.vercel.app","https://brainly-100x.netlify.app"],
 
   credentials: true,
 };
